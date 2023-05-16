ff67a30246a8b06d561f0758416edd8312ee68f1
blackman <lujun19950@gmail.com>
2023-05-16 20:16:57 +0800

    append & delete button code

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -7,8 +7,8 @@
   <span class="custom-tree-node" slot-scope="{ node, data }">
     <span>{{ node.label }}</span>
     <span>
-      <el-button v-if="node.level <= 2" type="text" size="min" @click="() => append(data)" >Append</el-button>
-      <el-button v-if="node.childrenNodes.length == 0" type="text" size="min"  @click="() => remove(node, data)">Delete</el-button>
+      <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)" >Append</el-button>
+      <el-button v-if="node.childNodes.length == 0" type="text" size="mini"  @click="() => remove(node, data)">Delete</el-button>
     </span>
   </span>
  </el-tree>
